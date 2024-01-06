70e592741065fa0c385fd2b5738bf6f3b5cabdf0
Adamddour <106589116+Adamddour@users.noreply.github.com>
2024-01-06 22:25:22 +0100

    22:25

--- Datenbanken/src/datenbanken/CLApplication.java
+++ Datenbanken/src/datenbanken/CLApplication.java
@@ -10,25 +10,15 @@ public class CLApplication {
       
         // Schritt 1: Erstellen der Database-Instanz
         Database database = new Database();
-        database.setJdbcUrl("jdbc:postgresql://localhost:5432/Vorlesung");
+        database.setJdbcUrl("jdbc:postgresql://localhost:5432/postgres");
         // Schritt 2: Öffnen der Verbindung zur Datenbank
         database.openConnection();
 
         // Fügen Sie hier Ihren Code für Datenbankoperationen ein
 
-        // Schritt 3: Schließen der Verbindung zur Datenbank
-        // Aufgabe Seite 5 Ausführen von SQL-Statements (ohne Rückgabewert) 
-       String createSchemaSQL = "CREATE SCHEMA IF NOT EXISTS neuesSchema";
-        String setSchemaSQL = "SET search_path TO neuesSchema";
-        String createTableSQL = "CREATE TABLE IF NOT EXISTS neuesSchema.xyz (id SERIAL PRIMARY KEY, name VARCHAR(255))";
-
-        database.dbQuery(createSchemaSQL);
-        database.dbQuery(setSchemaSQL);
-        database.dbQuery(createTableSQL);
         
-        database.dbQuery("CREATE DATABASE praktikum4");
-		database.dbQuery("CREATE SCHEMA IF NOT EXISTS versuch4;");
-		database.dbQuery("set SEARCH_PATH to versuch4;");
+		database.dbQuery("CREATE SCHEMA IF NOT EXISTS versuch5;");
+		database.dbQuery("set SEARCH_PATH to versuch5;");
 		database.dbQuery("CREATE TABLE mitarbeiter ( mid int, name text, vorname text, aid int);");
 		database.dbQuery("CREATE TABLE abteilung ( aid int, name text);");
 		database.dbQuery("INSERT INTO mitarbeiter VALUES (01,'John','Geddy',007); ");
@@ -59,4 +49,16 @@ public class CLApplication {
     }
 }
 
+
+//Schritt 3: Schließen der Verbindung zur Datenbank
+// Aufgabe Seite 5 Ausführen von SQL-Statements (ohne Rückgabewert) 
+/*String createSchemaSQL = "CREATE SCHEMA IF NOT EXISTS neuesSchema";
+String setSchemaSQL = "SET search_path TO neuesSchema";
+String createTableSQL = "CREATE TABLE IF NOT EXISTS neuesSchema.xyz (id SERIAL PRIMARY KEY, name VARCHAR(255))";
+
+database.dbQuery(createSchemaSQL);
+database.dbQuery(setSchemaSQL);
+database.dbQuery(createTableSQL);*/
+
+//database.dbQuery("CREATE DATABASE praktikum4");
 
